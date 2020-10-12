c551d9e5614ef574d7ecc08c9ac421b97099116f
Júlio <35194@ufp.edu.pt>
2020-10-12 23:36:33 +0100

    Update 2 - pt.2
    Update 2 pt.2:
    -BD
    
    --12/10/2020

---
+++ BD/12/pg_env.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+REM The script sets environment variables helpful for PostgreSQL
+
+@SET PATH="C:\PostgreSQL\12\bin";%PATH%
+@SET PGDATA=C:\PostgreSQL\12\data
+@SET PGDATABASE=postgres
+@SET PGUSER=postgres
+@SET PGPORT=5432
+@SET PGLOCALEDIR=C:\PostgreSQL\12\share\locale
+
+                            
